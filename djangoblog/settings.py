6ab21c64650a5c598cbc538903b886765fe8914e
David <jdy.toh@gmail.com>
2018-07-01 21:52:36 +0900

    fix STATIC_ROOT

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'CollectedStatic')
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'Static'),
 )
